1126afef692abb6e2f5ebd8479d4fbb3e51b31a0
kigos21 <83705521+kigos21@users.noreply.github.com>
2022-07-30 22:17:27 +0800

    Created checkBal, deposit, withdraw methods on main method

---
+++ src/Account.java
@@ -0,0 +1,51 @@
+public class Account {
+    private String name;
+    private String pin = "mad123";
+    private double balance = 0;
+
+    public Account(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public double withdraw(double amount) {
+        this.balance -= amount;
+        return amount;
+    }
+
+    public double checkBal() {
+        return this.balance;
+    }
+
+    public void changePin(String pin) {
+        this.pin = pin;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,178 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void addInterest(Account account) {
+        if (account.getBalance() > 10000)
+            account.setBalance(account.getBalance()*1.05);
+    }
+
+    public static void chargePenalty(Account account) {
+        if (account.getBalance() < 5000)
+            account.setBalance(account.getBalance()*0.98);
+    }
+
+    public static Account createAccount() {
+        System.out.println("[ Creating an account... ] ");
+
+        String name;
+        while (true) {
+            System.out.print("Name of the account \n>> ");
+            name = scanner.next().toLowerCase().trim();
+            if (name.isBlank()) {
+                System.out.println("Invalid name, try again.");
+                continue;
+            }
+            break;
+        }
+
+        double balance;
+        while (true) {
+            System.out.print("Initial amount of deposit (minimum of Php 5000) \n>> Php ");
+            try {
+                balance = scanner.nextDouble();
+                if (balance < 5000) {
+                    System.out.println("Minimum of Php 5000, try again. ");
+                    continue;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Invalid input, try again. ");
+                scanner.nextLine();
+                continue;
+            }
+            scanner.nextLine();
+            break;
+        }
+        return new Account(name, balance);
+    }
+
+    public static boolean askPin(Account account) {
+        int tries = 0;
+        String input;
+
+        while (tries != 3) {
+            System.out.println("[ Login ] ");
+            System.out.print("Input your PIN: ");
+            try {
+                input = scanner.nextLine();
+                if (!(input.equals(account.getPin()))) {
+                    tries += 1;
+                    System.out.format("Wrong PIN! %d tries left... ", 3-tries);
+                    System.out.println();
+                    continue;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static char askTransact() {
+        char[] validInput = {'c','d','w','h'};
+        char transactChoice;
+        while (true) {
+            System.out.println("================ \nMadATM is Online \n================ ");
+            System.out.println("[C]heck Balance ");
+            System.out.println("[D]eposit Cash ");
+            System.out.println("[W]ithdraw Cash ");
+            System.out.println("C[H]hange PIN ");
+            System.out.println();
+            System.out.print("Transaction [C,D,W,H]: ");
+            transactChoice = scanner.nextLine().toLowerCase().charAt(0);
+
+            for (char c: validInput)
+                if (c == transactChoice)
+                    return transactChoice;
+
+            System.out.println("Invalid choice, try again. ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Account[] accounts = new Account[1];
+        boolean initialized = false;
+
+        while (true) {
+            System.out.println("================== \nWelcome to MadATM! \n================== ");
+            if (!initialized) {
+                Account account = createAccount();
+                accounts[0] = account;
+                System.out.println("Account created! Proceed to login. ");
+                initialized = true;
+                continue;
+            }
+
+            Account account = accounts[0];
+            if (askPin(account)) {
+                System.out.println("Logging you in... ");
+            } else {
+                System.out.println("3 failed attempts, forcing system shutdown... ");
+                System.exit(0);
+            }
+
+            while (true) {
+                char transact = askTransact();
+                if (transact == 'c') {
+                    System.out.println("Account name:    " + account.getName());
+                    System.out.println("Account balance: Php " + account.checkBal());
+
+                } else if (transact == 'd') {
+                    while (true) {
+                        System.out.println("Account balance: Php " + account.getBalance());
+                        System.out.print("Enter the amount of deposit: Php ");
+                        try {
+                            double deposit_amt = scanner.nextDouble();
+                            if ((deposit_amt <= 0))
+                                throw new Exception();
+                            account.deposit(deposit_amt);
+                        } catch (Exception e) {
+                            System.out.println("Invalid input, try again. ");
+                            scanner.nextLine();
+                            continue;
+                        }
+                        System.out.println("[ updated ] Account balance: Php " + account.getBalance());
+                        scanner.nextLine();
+                        break;
+                    }
+
+                } else if (transact == 'w') {
+                    while (true) {
+                        System.out.println("Account balance: Php " + account.getBalance());
+                        System.out.print("Enter the amount to be withdrawn: Php ");
+                        try {
+                            double withdraw_amt = scanner.nextDouble();
+                            if ((withdraw_amt < 0) || withdraw_amt > account.getBalance())
+                                throw new Exception();
+                            account.withdraw(withdraw_amt);
+                        } catch (Exception e) {
+                            System.out.println("Invalid input or insufficient funds, try again. ");
+                            scanner.nextLine();
+                            continue;
+                        }
+                        System.out.println("[ updated ] Account balance: Php " + account.getBalance());
+                        scanner.nextLine();
+                        break;
+                    }
+
+                } else {
+                    System.out.print("Input your CURRENT PIN: ");
+                    String currentPinInput = scanner.nextLine();
+                    System.out.print("Input your NEW PIN: ");
+                    String newPinInput = scanner.nextLine();
+                    System.out.println("Verify NEW PIN: ");
+                    String verificationInput = scanner.nextLine();
+                    if (currentPinInput.equals(account.getPin())) {
+
+                    }
+                }
+            }
+        }
+    }
+
+}
